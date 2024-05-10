e5733195bca569a0d884e87c7de9b841a52eee56
mahmudul474 <mdmahmudulla474@gmail.com>
2024-05-10 22:28:53 +0600

    database add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors= require("cors");
+const { MongoClient } = require("mongodb");
 
 //middleware
 app.use(express.json());
@@ -10,58 +11,10 @@ app.use(cors());
 
 
 
-//mOSRumMFH1wcNdCk
-//amin
-
-
-
-
-
-
-
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-//   "mongodb+srv://amin:mOSRumMFH1wcNdCk@softopark.ockrkce.mongodb.net/?retryWrites=true&w=majority&appName=softopark";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-     
- 
-
-// const myDB = client.db("myDB");
-// const menucollection= myDB.collection("pizzaMenu");
-
-
-
-//    app.post("/data", (req, res) => {
-//      const data = req.body;
-//      const result = menucollection.insertOne(data);
-//      res.json({msg:"data inserted successfully", data: result});
-//    });
-
-     
-//   } finally {
-    
-//   }
-// }
-// run().catch(console.dir);
-
-
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-const { MongoClient } = require("mongodb");
+
 
 // Replace the uri string with your connection string.
 const uri = "<connection string uri>";
@@ -79,8 +32,7 @@ async function run() {
 
     console.log(movie);
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+     
   }
 }
 run().catch(console.dir);
