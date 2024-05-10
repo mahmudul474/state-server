fcbec3717996b45dc5070e1ea94f515e658e6bfc
mahmudul474 <mdmahmudulla474@gmail.com>
2024-05-10 23:59:30 +0600

    d

--- index.js
+++ index.js
@@ -1,62 +1,29 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
+const port = 3000;
+const cors= require("cors");
+ 
+ 
+//middleware
 app.use(express.json());
+app.use(cors());
+ 
+ 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.x52xd2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const dataFilePath = "./data.json";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
 
-// Helper function to read data from the JSON file
-const readDataFromFile = () => {
+async function run() {
   try {
-    const data = fs.readFileSync(dataFilePath);
-    return JSON.parse(data);
-  } catch (error) {
-    console.error("Error reading data from file:", error);
-    return [];
+    
+
+console.log("before starting")
+
+  } finally {
+    
   }
-};
-
-// Helper function to write data to the JSON file
-const writeDataToFile = (data) => {
-  try {
-    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-  } catch (error) {
-    console.error("Error writing data to file:", error);
-  }
-};
-
-// GET all items
-app.get("/api/items", (req, res) => {
-  const items = readDataFromFile();
-  res.json(items);
-});
-
-// GET item by ID
-app.get("/api/items/:id", (req, res) => {
-  const itemId = parseInt(req.params.id);
-  const items = readDataFromFile();
-  const item = items.find((item) => item.id === itemId);
-  if (!item) return res.status(404).send("Item not found");
-  res.json(item);
-});
-
-// POST a new item
-app.post("/api/items", (req, res) => {
-  const newItem = {
-    id: Date.now(),
-    name: req.body.name,
-  };
-  const items = readDataFromFile();
-  items.push(newItem);
-  writeDataToFile(items);
-  res.status(201).json(newItem);
-});
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-
-//check dara 
+}
+run().catch(console.dir);
