1d6fa912fdbe10afb703a8467c17bf3e0238c999
mahmudul474 <mdmahmudulla474@gmail.com>
2024-05-10 22:46:31 +0600

    done

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors= require("cors");
-const { MongoClient } = require("mongodb");
-
+ 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+
+
+const  main=()=>{
+  
+ app.get("/hello", (req, res)=>{
+    res.send("world! bad server this  one ");
+ }) 
+
+
+}
+  main();
 
 
 
@@ -14,28 +25,6 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-
-// Replace the uri string with your connection string.
-const uri = "<connection string uri>";
-
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    const database = client.db("sample_mflix");
-    const movies = database.collection("movies");
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: "Back to the Future" };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-  } finally {
-     
-  }
-}
-run().catch(console.dir);
 
 
 
